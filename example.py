8fc99fa18c4ce65a97f1942cd140f50dd9c6b722
NoFate35 <vsokol35@mail.ru>
2025-01-22 22:25:51 +0300

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   Makefile 	modified:   example.py 	modified:   pyproject.toml 	modified:   uv.lock

--- example.py
+++ example.py
@@ -1,8 +1,26 @@
 from flask import Flask
+from flask import jsonify
+
+from faker import Faker
+
+fake = Faker()
+fake.seed_instance(1234)
+
+domains = [fake.domain_name() for i in range(10)]
+phones = [fake.phone_number() for i in range(10)]
 
 # Это callable WSGI-приложение
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Welcome to Flask!'
+
+@app.route("/")
+def index():
+    return "go to the /phones or /domains" 
+
+@app.route('/phones')
+def get_phones():
+    return jsonify(phones)
+
+@app.route("/domains")
+def get_domains():
+    return jsonify(domains)
