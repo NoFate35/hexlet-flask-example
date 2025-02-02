e109548d679b913413abb158a5c7684081647ce5
NoFate35 <vsokol35@mail.ru>
2025-02-02 23:04:37 +0300

    Changes to be committed: 	modified:   app.py 	modified:   templates/courses/index.html 	modified:   tests/test_app.py

--- app.py
+++ app.py
@@ -9,9 +9,20 @@ from flask import (
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+app.secret_key = "secret_key"
+
+#app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 # BEGIN (write your solution here)
+@app.get('/')
+def get_courses():
+    messages = get_flashed_messages(with_categories = True)
+    return render_template('courses/index.html', messages=messages)
 
+@app.post('/courses/')
+def redirect_courses():
+    flash('Course Added ', 'success')
+    return redirect(url_for('get_courses'))
 # END
