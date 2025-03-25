4d995e699f809b8641aa98f61a47edeedd207f6a
NoFate35 <vsokol35@mail.ru>
2025-03-26 00:47:45 +0300

    Changes to be committed: 	modified:   build.sh 	modified:   poetry.lock 	modified:   pyproject.toml

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
-
+:'
 #!/bin/bash
 sudo service postgresql start
-psql flaskdb < init.sql
 psql -a -d $DATABASE_URL flaskdb
-poetry build --no-root
-:'
-#!/data/data/com.termux/files/usr/bin/bash
-pg_ctl -D $PREFIX/var/lib/postgresql start
-psql -a -d $DATABASE_URL flaskdb
+export DATABASE_URL="postgresql://u0_a441:@localhost/flaskdb"
 psql flaskdb < init.sql
 '
+
+#!/data/data/com.termux/files/usr/bin/bash
+pg_ctl -D $PREFIX/var/lib/postgresql start
+export DATABASE_URL="postgresql://u0_a441:@localhost/flaskdb"
+psql flaskdb < init.sql
+
