d5e610f220fa8c03139e15af3d69992dad88ab24
NoFate35 <vsokol35@mail.ru>
2025-03-26 00:26:11 +0300

    Changes to be committed: 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-:'
+
 #!/bin/bash
 sudo service postgresql start
 psql flaskdb < init.sql
 psql -a -d $DATABASE_URL flaskdb
-'
+poetry build --no-root
+:'
 #!/data/data/com.termux/files/usr/bin/bash
 pg_ctl -D $PREFIX/var/lib/postgresql start
 psql -a -d $DATABASE_URL flaskdb
 psql flaskdb < init.sql
+'
