8eba808fde907445976f7bb47396252dffd22f4d
NoFate35 <vsokol35@mail.ru>
2025-03-26 01:05:57 +0300

    Changes to be committed: 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,10 @@
 #!/bin/bash
 sudo service postgresql start
 psql -a -d $DATABASE_URL flaskdb
-export DATABASE_URL="postgresql://u0_a441:@localhost/flaskdb"
 psql flaskdb < init.sql
-'
 
+:'
 #!/data/data/com.termux/files/usr/bin/bash
 pg_ctl -D $PREFIX/var/lib/postgresql start
-export DATABASE_URL="postgresql://u0_a441:@localhost/flaskdb"
 psql flaskdb < init.sql
-
+'
