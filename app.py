afc70bbc8247ab1a72210e79ac87a648f2a9f584
NoFate35 <vsokol35@mail.ru>
2025-04-02 22:29:52 +0300

    Changes to be committed: 	modified:   app.py 	modified:   poetry.lock 	modified:   pyproject.toml 	modified:   templates/courses/index.html

--- app.py
+++ app.py
@@ -34,5 +34,15 @@ def index():
 
 
 # BEGIN (write your solution here)
+debug = app.logger.debug
 
+@app.route('/posts', methods=['GET', 'POST'])
+def posts_route():
+    query = request.args.get('query')
+    posts = repo.get_all_posts()
+    debug("query: %s, posts: %s", query, posts)
+    if request.method == 'GET':
+        return render_template('courses/index.html')
+    if request.method == 'POST':
+        return render_template('courses/index.html')
 # END
