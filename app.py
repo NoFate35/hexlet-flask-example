5bc5aca52e9d75d207bc907aca09e4636e6ec0de
NoFate35 <vsokol35@mail.ru>
2025-04-03 13:42:42 +0300

    Changes to be committed: 	modified:   app.py 	modified:   templates/courses/index.html

--- app.py
+++ app.py
@@ -36,13 +36,16 @@ def index():
 # BEGIN (write your solution here)
 debug = app.logger.debug
 
-@app.route('/posts', methods=['GET', 'POST'])
+@app.route('/posts')
 def posts_route():
-    query = request.args.get('query')
+    query = request.args.get('term')
     posts = repo.get_all_posts()
-    debug("query: %s, posts: %s", query, posts)
-    if request.method == 'GET':
-        return render_template('courses/index.html')
-    if request.method == 'POST':
-        return render_template('courses/index.html')
+    filter_posts = []
+    if query:
+        for post in posts:
+            if (query in post.title) or (query in post.content):
+                debug("query: %s, post.title: %s \n post.body: %s", query, post.title, post.content)
+                filter_posts.append(post)
+                posts = filter_posts
+    return render_template('courses/index.html', posts=posts)
 # END
