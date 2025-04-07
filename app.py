4ee2996a16d0b49ce73d6621e01093e001bde488
NoFate35 <vsokol35@mail.ru>
2025-04-07 16:17:00 +0300

    Changes to be committed: 	modified:   app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import secrets
 from dotenv import load_dotenv
 from flask import Flask, abort, flash, redirect, render_template, request, session, url_for
-from repository import Comment, CommentStatus, Repository, generate_posts, get_comment
+from repository import Comment, CommentStatus, Repository, generate_posts
 from validator import check_spam, check_triggers
 
 secret = secrets.token_urlsafe(32)
@@ -42,7 +42,6 @@ def view_post(post_id):
         abort(404)
 
     post_comments = repo.get_comments_by_post(post_id)
-    #debug("post_comments view_post: %s", post_comments)
     return render_template("courses/view.html", post=post, comments=post_comments)
 
 
@@ -64,15 +63,20 @@ def add_comment(post_id):
         status = comment_status
         )
         repo.save_comment(comment)
-        post_comments = repo.get_comments_by_post(post_id)
-        #debug("post comments add_comments: %s", post_comments)
     return redirect(url_for('view_post', post_id=post_id))
 
 
 @app.route("/moderate/<uuid:comment_id>", methods=['POST'])
 def moderate_comment(comment_id):
-    comment = get_comment(comment_id)
+    comment = repo.get_comment(comment_id)
     action = request.form.to_dict()
+    if action['action'] == 'approve':
+        comment = Comment(
+                            post_id = comment.post_id,
+                            text = comment.text,
+                            status = CommentStatus.APPROVED
+        )
+        repo.save_comment(comment)     
     debug("comment: %s, action: %s", comment, action)
     return redirect(url_for('moderate_comments'))
 # END
@@ -83,11 +87,11 @@ def moderate_comments():
     waiting_comments = repo.get_waiting_comments()
     #debug("waiting comments: %s", waiting_comments)
 
-    """comments_with_context = []
+    comments_with_context = []
     for comment in waiting_comments:
         post = repo.get_post(comment.post_id)
         if post:
             context = {"post_title": post.title, "comment": comment}
-            comments_with_context.append(context)"""
+            comments_with_context.append(context)
 
     return render_template("courses/moderate.html", comments=waiting_comments)
